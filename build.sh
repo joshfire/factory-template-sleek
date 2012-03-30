3af408cdf228c0708003da8eda9805f66742f428
stephan83 <stephan.florquin@gmail.com>
2012-03-30 10:28:50 +0200

    Added tablet index

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 cd app/
 node joshfire-framework/scripts/optimize.js ios app.phone
 node joshfire-framework/scripts/optimize.js googletv app.tv
+node joshfire-framework/scripts/optimize.js ios app.tablet
