eb584b5120430087e628e289abd875c085101843
stephan83 <stephan.florquin@gmail.com>
2012-04-10 14:33:46 +0200

    refactoring

--- app/spot.js
+++ app/spot.js
@@ -5,18 +5,28 @@
 */
 
 define([
-  'joshlib!factorycollection',
+  'joshlib!collection',
   'joshlib!ui/item',
   'joshlib!ui/imageloader',
+  'joshlib!router',
   'joshlib!vendor/underscore'],
-function(FactoryCollection, Item, ImageLoader, _) {
+function(Collection, Item, ImageLoader, Router, _) {
 
-  var Spot = {
+  var Spot = function() {
+    _.bindAll(this, 'initialize',  'setColor', 'slugify');
+  };
 
-    extend: function(obj) {
-      _.extend(this, obj);
-    },
+  Spot.extend = function(properties) {
+    var Class = function() {
+      Spot.call(this);
+    };
 
+    _.extend(Class.prototype, Spot.prototype);
+    _.extend(Class.prototype, properties);
+    return Class;
+  },
+
+  _.extend(Spot.prototype, {
     initialize: function() {
       this.title = Joshfire.factory.config.app.name;
       this.backgroundURL = Joshfire.factory.config.template.options.backgroundurl;
@@ -25,6 +35,60 @@ function(FactoryCollection, Item, ImageLoader, _) {
       this.setColor(Joshfire.factory.config.template.options.color || 'blue');
 
       document.title = this.title;
+
+      //
+      // Sections
+      //
+      var datasources = Joshfire.factory.getDataSource('main');
+      var sections = new Array(datasources.children.length);
+      var loaded = 0;
+
+      _.forEach(datasources.children, _.bind(function(datasource, index) {
+        var name = datasource.name;
+        var slug = this.slugify(name.toLowerCase());
+        var collection = new Collection([], {
+          dataSource: datasource,
+          dataSourceQuery: {}
+        });
+        var outputType;
+
+        datasource.getDesc(_.bind(function(err, desc) {
+          switch(desc.outputType) {
+            case 'Article/Status':
+            outputType = 'statuses';
+            break;
+            case 'NewsArticle':
+            case 'BlogPosting':
+            outputType = 'news';
+            break;
+            case 'Event':
+            outputType = 'events';
+            break;
+            case 'ImageObject':
+            outputType = 'photos';
+            break;
+            case 'VideoObject':
+            outputType = 'videos';
+            break;
+            default:
+            outputType = 'other';
+          }
+
+          sections[index] = {
+            name: name,
+            slug: index + '--' + slug,
+            outputType: outputType,
+            collection: collection
+          };
+
+          if(++loaded === datasources.children.length) {
+            var views = this.createViews(sections);
+            var controllers = this.createRoutes(sections, views);
+            var router = Router(controllers);
+            router.historyStart();
+          }
+        }, this));
+      }, this));
     },
 
     //
@@ -35,9 +99,25 @@ function(FactoryCollection, Item, ImageLoader, _) {
     },
 
     //
+    // Creates views
+    //
+    createViews: function(sections) {
+      return {};
+    },
+
+    //
+    // Creates routes
+    //
+    createRoutes: function(sections, views) {
+      return {};
+    },
+
+    //
     // Creates a list item view based on the type of the item.
     //
     itemFactory: function(section) {
+      var self = this;
+
       return function(model, offset) {
         var item = model.toJSON();
         var type = item['@type'] || item.itemType;
@@ -50,7 +130,7 @@ function(FactoryCollection, Item, ImageLoader, _) {
             offset: offset,
             templateEl: '#template-image-item',
             getImageUrl: function() {
-              return Spot.getThumbnail(item, offset);
+              return self.getThumbnail(item, offset);
             }
           });
           break;
@@ -116,7 +196,7 @@ function(FactoryCollection, Item, ImageLoader, _) {
       text = text.replace(/\s/gi, "-");
       return text;
     }
-  };
+  });
 
   return Spot;
 });
--- app/spot.tablet.js
+++ app/spot.tablet.js
@@ -4,13 +4,240 @@
 
 */
 
-define(['spot', 'joshlib!utils/dollar'], function(Spot, $) {
+define(['spot', 'joshlib!ui/list',
+  'joshlib!ui/item',
+  'joshlib!ui/factorymedia',
+  'ui/imagegallery','joshlib!utils/dollar', 'joshlib!vendor/underscore'], function(Spot, List, Item, FactoryMedia, ImageGallery, $, _) {
 
-  Spot.extend({
+  return Spot.extend({
 
     setColor: function(color) {
       $('#color').remove();
       $('head').append('<link id="color" rel="stylesheet" href="css/tablet.' + color + '.css" type="text/css">');
+    },
+
+    //
+    // Creates views
+    //
+    createViews: function(sections) {
+      //
+      // Toolbar
+      //
+      var sectionCollection = new Backbone.Collection();
+
+      for(var i = 0; i < sections.length; i++) {
+        section = sections[i];
+        sectionCollection.add({
+          name: section.name,
+          linkURL: '#' + section.slug,
+          outputType: section.outputType,
+          slug: section.slug
+        });
+      }
+
+      var toolbar = new List({
+        el: '#toolbar',
+        templateEl: '#template-toolbar',
+        itemTemplateEl: '#toolbar-item'
+      });
+
+      toolbar.setCollection(sectionCollection);
+      toolbar.render();
+
+      //
+      // Views
+      //
+      var views = {};
+      var $cards = $('#cards');
+
+      _.forEach(sections, _.bind(function(section) {
+        var list;
+        var detail;
+
+        switch(section.outputType) {
+          case 'statuses':
+          list = new List({
+            templateEl: '#template-list-view',
+            scroller: true,
+            itemFactory: this.itemFactory(section),
+            collection: section.collection,
+            className: section.outputType + ' left-panel hashed-list'
+          });
+          detail = new Item({
+            templateEl: '#template-status',
+            scroller: true,
+            className: 'right-panel'
+          });
+          break;
+          case 'news':
+          list = new List({
+            templateEl: '#template-list-view',
+            scroller: true,
+            itemFactory: this.itemFactory(section),
+            collection: section.collection,
+            className: section.outputType + ' left-panel simple-list'
+          });
+          detail = new Item({
+            templateEl: '#template-news',
+            scroller: true,
+            className: 'right-panel'
+          });
+          break;
+          case 'events':
+          list = new List({
+            templateEl: '#template-list-view',
+            scroller: true,
+            itemFactory: this.itemFactory(section),
+            collection: section.collection,
+            className: section.outputType + ' left-panel hashed-list'
+          });
+          detail = new Item({
+            templateEl: '#template-event',
+            scroller: true,
+            className: 'right-panel'
+          });
+          break;
+          case 'videos':
+          list = new List({
+            templateEl: '#template-list-view',
+            scroller: true,
+            itemFactory: this.itemFactory(section),
+            collection: section.collection,
+            className: section.outputType + ' left-panel simple-list'
+          });
+          detail = new FactoryMedia({
+            templateEl: '#template-video',
+            scroller: true,
+            className: 'right-panel',
+            mediaOptions: {
+              strategy: 'html5',
+              width: '100%'
+            }
+          });
+          break;
+          case 'photos':
+          list = new ImageGallery({
+            templateEl: '#template-list-view',
+            scroller: true,
+            itemFactory: this.itemFactory(section),
+            collection: section.collection,
+            className: section.outputType + ' section mosaic-list'
+          });
+          break;
+        }
+
+        if(list) {
+          list.hide();
+          list.render();
+          views[section.slug] = list;
+          $cards.append(list.el);
+        }
+
+        if(detail) {
+          detail.hide();
+          views[section.slug + 'Detail'] = detail;
+          $cards.append(detail.el);
+        }
+      }, this));
+
+      return views;
+    },
+
+    //
+    // Creates routes
+    //
+    createRoutes: function(sections, views) {
+      var controllers = {
+        routes: {
+          '': 'home'
+        },
+
+        home: function() {
+          if(sections.length) {
+            controllers[sections[0].slug]();
+          }
+        }
+      };
+      var $title = $('#title');
+      var $toolbar = $('#toolbar');
+
+      _.forEach(sections, function(section) {
+        controllers.routes[section.slug] = section.slug;
+
+        // List route
+        controllers[section.slug] = function() {
+          var view = views[section.slug];
+          _.forEach(views, function(child) {
+            if(child !== view) {
+              child.hide();
+            }
+          });
+          view.show();
+          $('iframe').remove();
+          document.body.id = section.outputType;
+          $title.html(section.name);
+          $toolbar.find('.active').removeClass('active');
+          $toolbar.find('.section-' + section.slug).addClass('active');
+
+          if(section.outputType !== 'photos') {
+            var detail = views[section.slug + 'Detail'];
+            detail.show();
+          }
+
+          section.collection.fetch({
+            success: function() {
+              view.render();
+
+              if(section.collection.length !== 0 && section.outputType !== 'photos') {
+                detail.setModel(section.collection.at(0));
+                detail.render();
+                view.$('.active').removeClass('active');
+                $(view.$('li')[0]).addClass('active');
+              }
+            }
+          });
+        }
+
+        // Detail route
+        if(section.outputType !== 'photos') {
+          controllers.routes[section.slug + '/:offset'] = section.slug + 'Detail';
+
+          controllers[section.slug + 'Detail'] = function(offset) {
+            var view = views[section.slug];
+            var detail = views[section.slug + 'Detail'];
+            offset = parseInt(offset, 10);
+            view.show();
+            $('iframe').remove();
+            document.body.id = section.outputType;
+            $title.html(section.name);
+            $toolbar.find('.active').removeClass('active');
+            $toolbar.find('.section-' + section.slug).addClass('active');
+            detail.show();
+
+            if(section.collection.length === 0) {
+              section.collection.fetch({
+                success: function() {
+                  view.render();
+
+                  if(section.collection.length > offset) {
+                    detail.setModel(section.collection.at(offset));
+                    detail.render();
+                    view.$('.active').removeClass('active');
+                    $(view.$('li')[offset]).addClass('active');
+                  }
+                }
+              });
+            } else if(section.collection.length > offset) {
+              detail.setModel(section.collection.at(offset));
+              detail.render();
+              view.$('.active').removeClass('active');
+              $(view.$('li')[offset]).addClass('active');
+            }
+          };
+        }
+      });
+
+      return controllers;
     },
 
     //
@@ -54,6 +281,4 @@ define(['spot', 'joshlib!utils/dollar'], function(Spot, $) {
       return item.contentURL;
     }
   });
-
-  return Spot;
 });
