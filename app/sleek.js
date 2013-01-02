eeea2640d0574ac98d1dd2cea204ed396fa82665
Arthur Jamain <arthur.jamain@gmail.com>
2013-01-02 18:04:34 +0100

    &'s should not appear in URIs anymore

--- app/sleek.js
+++ app/sleek.js
@@ -949,6 +949,7 @@ function (Collection, DynamicContainer, Item, List, CardPanel, FadeInPanel, Fact
       text = text.replace(/[^\-a-zA-Z0-9,&\s]+/ig, '');
       text = text.replace(/-/gi, "_");
       text = text.replace(/\s/gi, "-");
+      text = text.replace(/&/gi, "-");
       return text;
     }
   });
