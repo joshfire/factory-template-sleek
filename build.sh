418dece3ced2399cca4fa77dc8a0999fa6e87ad2
Clément Vollet <clement@joshfire.com>
2013-02-20 16:57:13 +0100

    Update build script: remove optimized js from source tree; add a new build folder that contains the tempalte;

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-# Optimize versions
 cd app/
+
+# Optimize versions
 node joshfire-framework/scripts/optimize.js ios app.phone
 node joshfire-framework/scripts/optimize.js android app.phone
 node joshfire-framework/scripts/optimize.js tv app.tv
@@ -8,22 +9,37 @@ node joshfire-framework/scripts/optimize.js ios app.tablet
 node joshfire-framework/scripts/optimize.js android app.tablet
 node joshfire-framework/scripts/optimize.js ios app.desktop
 
-# Pack Samsung Version
 cd ..
+
+# Build folder
+rm -fr build
+mkdir -p build/app/
+
+echo "Moving files around..."
+
+# Copy resources to the build folder
+cp -r app/{index.*.optimized.html,css,images,ui,vendor,lang} build/app/
+cp app/todate.js build/app/
+cp app/prettystatus.js build/app/
+# cp config.xml build/config.xml
+cp package.json build/
+
+# Move optimized js files
+mv app/app.*.optimized.js build/app/
+
+# Pack Samsung Version
 mkdir samsung_export
-cp -r app/css samsung_export
-cp -r app/images samsung_export/
-cp -r app/ui samsung_export/
-cp -r app/vendor samsung_export/
-cp -r app/lang samsung_export/
-cp app/index.samsung.optimized.html samsung_export/index.html
-cp app/app.tv.samsung.samsungtv.optimized.js samsung_export/
+cp -r build/app/{css,images,ui,vendor,lang} samsung_export
+cp build/app/index.samsung.optimized.html samsung_export/index.html
+cp build/app/app.tv.samsung.samsungtv.optimized.js samsung_export/
 cp app/todate.js samsung_export/
 cp app/prettystatus.js samsung_export/
 cp config.xml samsung_export/config.xml
 rm tasks/server/public/sleek.zip
 out="../tasks/server/public/sleek.zip"
 cd samsung_export
+
+echo "Archiving samsung_export..."
 zip -r $out ./*
 cd ..
 rm -rf samsung_export
