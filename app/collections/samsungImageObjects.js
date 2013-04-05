59d69b807df2e7921e668b7c1281162d7d3331fa
François Daoust <francois@joshfire.com>
2013-04-05 17:49:48 +0200

    Samsung TV image collection now derives from joshlib!collection
    The specific image collection is useful to filter out images that
    are too large as Samsung TV models tend to crash when requested to
    render such images.
    
    The collection used to derive from Backbone.Collection directly
    but the "joshlib!collection" class now adds useful logic to fetch
    more items in a collection and to trigger load events when new items
    are fetched.

--- app/collections/samsungImageObjects.js
+++ app/collections/samsungImageObjects.js
@@ -7,133 +7,34 @@
  */
 
 define([
-  'joshlib!vendor/backbone',
+  'joshlib!collection',
   'joshlib!vendor/underscore'
-], function (Backbone, _) {
-
-  var newCol = Backbone.Collection.extend({
-
-    //TODO children
+], function (Collection, _) {
+  var filteredCollection = Collection.extend({
     /**
-     * Initializes the collection.
-     *
-     * Note that the collection is not built from a list of models but rather
-     * from a datasource, given as parameter in the "options" hash.
-     *
-     * @function
-     * @param {Object} models Defined to ensure compatibility with BackBone,
-     *   but unused in practice.
-     * @param {Object} options Options hash. The function sets the underlying
-     *   datasource if it finds a "dataSource" property, and sets base query
-     *   options if it finds a "dataSourceQuery" property.
+     * Overrides base sync method to filter out entries whose images are
+     * too large, because such images often result in a crash when the app
+     * attempts to render them on a Samsung TV (model 2011 at least).
      */
-    initialize: function(models, options) {
-      this.ds = null;
-      this.dsq = {};
-
+    sync: function (method, model, options) {
       options = options || {};
-      if (options.dataSource) {
-        this.setDataSource(options.dataSource);
-      }
-      if (options.dataSourceQuery) {
-        this.setDataSourceQuery(options.dataSourceQuery);
-      }
-    },
-
-    /**
-     * Sets the datasource that the collection wraps.
-     *
-     * @function
-     * @param {Object} ds Factory datasource object, as returned by
-     *   a call to Joshfire.factory.getDataSource in the Joshfire Factory.
-     */
-    setDataSource: function(ds) {
-      this.ds = ds;
-    },
-
-    /**
-     * Sets base datasource query options.
-     *
-     * @function
-     * @param {Object} dsq Datsource query options. These options
-     *   may be extended with additional options in calls to "sync"
-     */
-    setDataSourceQuery: function(dsq) {
-      this.dsq = dsq;
-    },
-
-    //TODO import from paginated collection
-    /**
-     * Fetches the underlying datasource.
-     *
-     * Fetch is asynchronous. The function calls "options.success"
-     * or "options.error" when it's done.
-     *
-     * @function
-     * @param {String} method The CRUD method ("create", "read", "update",
-     *   or "delete"). Only "read" is supported for the time being.
-     * @param {Object} model The model to be saved (or the collection to
-     *   be read). Unused in practice.
-     * @param {Object} options Success and error callbacks, and all other
-     *   request options
-     */
-    sync: function(method, model, options) {
-      
-      /* 
-      * Defence : if !this.ds, better do nothing stupid 
-      * Something stupid : null.find()
-      */
-      if (!this.ds){
-        this.trigger('syncerror', { method: method, err: 'init failed, ds is null' });
-        if (options && options.error) options.error('init failed, ds is null');
-        return;
-
-      }
-
-      options = options || {};
-      var queryOptions = {};
-      if (this.dsq) {
-        queryOptions = _.extend(queryOptions, this.dsq);
-      }
-      if (options.dataSourceQuery) {
-        queryOptions = _.extend(queryOptions, options.dataSourceQuery);
-      }
-
-      if (method === 'read') {
-        this.trigger('syncstarted', { method: method });
-        this.ds.find(queryOptions, _.bind(function (err, data) {
-          if (err) {
-            this.trigger('syncerror', { method: method, err: err });
-            if (options.error) options.error(err);
-            return;
-          }
-
-          if (this.ds.children) {
-            var entries = [];
-            entries = Array.prototype.concat.apply(entries, data.entries.map(function (entry) {
-              console.log(entry.entries);
-              return entry.entries;
-            }));
-
-            if (options.success) options.success(entries);
-          } else if (options.success) {
-
-            data.entries = _.filter(data.entries, function(el) {
-              if(el.width > 1024)
-                return false;
-              return true;
-            });
-
-            options.success(data.entries || []);
-          }
-
-          this.trigger('syncsuccess', {method: method});
-        }, this));
-      }
+      var success = options.success || function () {};
+      options.success = function (model, entries, options) {
+        // The goal of the following code is to update the "entries" array
+        // in place to filter out images that are too large ("in place" as
+        // in no call to concat, slice or other array methods that would
+        // create a copy of the array)
+        var copy = _.filter(entries, function (item) {
+          return (item.width <= 1024);
+        });
+        entries.splice(0, entries.length);
+        _.each(copy, function (item) {
+          entries.push(item);
+        });
+        success(model, entries, options);
+      };
+      Collection.prototype.sync.call(this, method, model, options);
     }
-
   });
-
-  return newCol;
-
+  return filteredCollection;
 });
